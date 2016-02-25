070530a8fbd2ed9e837cc1a7043fc4bdb1ccb6f2
Luís R <goblindegook@users.noreply.github.com>
2016-02-25 18:18:05 +0000

    Added oEmbed fixes

--- wp-content/mu-plugins/https.php
+++ wp-content/mu-plugins/https.php
@@ -8,15 +8,44 @@
  * License:     GPL-2.0+
  */
 
+namespace logoscon\WP\HTTPS;
+
 /**
  * Force URLs in srcset attributes into HTTPS scheme.
  *
  * @param     array    $sources    Source data to include in the 'srcset'.
  * @return    array                Possibly-modified source data.
  */
-\add_filter( 'wp_calculate_image_srcset', function( $sources ) {
+function wp_calculate_image_srcset( $sources ) {
 	foreach ( $sources as &$source ) {
 		$source['url'] = \set_url_scheme( $source['url'], \is_ssl() ? 'https' : 'http' );
 	}
 	return $sources;
-} );
+}
+
+/**
+ * Replace http:// with https:// in the embed code (before caching).
+ * 
+ * @param string $data The returned oEmbed HTML.
+ * @param string $url  URL of the content to be embedded.
+ * @param array  $args Optional arguments, usually passed from a shortcode.
+ */
+function oembed_result( $data, $url, $args ) {
+    return \is_ssl() ? preg_replace( '/http:\/\//', 'https://', $data ) : $data;
+}
+
+/**
+ * Replace http:// with https:// in the embed code (after caching).
+ * 
+ * @param mixed  $cache   The cached HTML result, stored in post meta.
+ * @param string $url     The attempted embed URL.
+ * @param array  $attr    An array of shortcode attributes.
+ * @param int    $post_ID Post ID.
+ */
+function embed_oembed_html( $cache, $url, $attr, $post_ID ) {
+    return \is_ssl() ? preg_replace( '/http:\/\//', 'https://', $cache ) : $cache;
+}
+
+\add_filter( 'wp_calculate_image_srcset', '\logoscon\WP\HTTPS\wp_calculate_image_srcset' 10, 1 );
+\add_filter( 'oembed_result',             '\logoscon\WP\HTTPS\secure_oembed_result',     10, 3 );
+\add_filter( 'embed_oembed_html',         '\logoscon\WP\HTTPS\secure_embed_oembed_html', 10, 4 );
